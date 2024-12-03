1262fb46cded5fa6f78cee60950c24829b74bfe1
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2024-12-03 11:52:01 -0500

    refactor(constants): extract constants to AppConstants class and update AuthFilter
    - Moved LOGIN_PATH, SIGNUP_PATH, INDEX_PATH, HOME_PATH, and USER_SESSION_ATTRIBUTE to AppConstants class
    - Updated AuthFilter to use constants from AppConstants
    
    This change improves code organization and maintainability by centralizing constant definitions.

---
+++ src/main/java/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class AppConstants {
+    public static final String LOGIN_PATH = "/login";
+    public static final String SIGNUP_PATH = "/signup";
+    public static final String INDEX_PATH = "index.jsp";
+    public static final String HOME_PATH = "home.jsp";
+    public static final String USER_SESSION_ATRIBUTE = "Usuario";
+}
--- src/main/java/filters/AuthFilter.java
+++ src/main/java/filters/AuthFilter.java
@@ -7,12 +7,9 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
+import static constants.AppConstants.*;
+
 public class AuthFilter implements Filter {
-    public static final String LOGIN_PATH = "/login";
-    public static final String SIGNUP_PATH = "/signup";
-    public static final String INDEX_PATH = "index.jsp";
-    public static final String HOME_PATH = "home.jsp";
-    public static final String USER_SESSION_ATRIBUTE = "Usuario";
 
     @Override
     public void doFilter(ServletRequest servletRequest,
